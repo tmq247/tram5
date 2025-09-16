e59459930c5c7a063b0a14495d53b1471814a73b
root <root@sg.vpn>
2025-09-16 04:57:40 +0000

    Clean repo, removed venv and large files

---
+++ HasiiMusic/__init__.py
@@ -0,0 +1,26 @@
+from HasiiMusic.core.bot import JARVIS
+from HasiiMusic.core.dir import dirr
+from HasiiMusic.core.git import git
+from HasiiMusic.core.userbot import Userbot
+from HasiiMusic.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = JARVIS()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
