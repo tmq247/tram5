a48020ea89f66d02030e392a15ac7b12b9184f7d
Hasindu Nagolla <116471812+hasindu-nagolla@users.noreply.github.com>
2025-09-16 19:39:50 +0530

    group configs

--- HasiiMusic/plugins/misc/restrict.py
+++ HasiiMusic/plugins/misc/restrict.py
@@ -2,6 +2,6 @@ from pyrogram import Client, filters
 import config
 
 @Client.on_message(filters.group)
-async def restrict_groups(client, message):
+async def auto_leave_unapproved(client, message):
     if message.chat.id not in config.ALLOWED_CHAT_IDS:
         await client.leave_chat(message.chat.id)
