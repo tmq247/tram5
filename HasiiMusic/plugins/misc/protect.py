39e01a0ad67eb279f0e7934997f62e75036069d0
Hasindu Nagolla <116471812+hasindu-nagolla@users.noreply.github.com>
2025-09-18 13:17:19 +0530

    auto-leave protection

--- HasiiMusic/plugins/misc/protect.py
+++ HasiiMusic/plugins/misc/protect.py
@@ -1,13 +1,14 @@
-from pyrogram import Client, filters
+from pyrogram import filters
+from HasiiMusic import app
 
-# Add your allowed group IDs here
+# Add only your allowed group IDs here:
 ALLOWED_CHAT_IDS = [
     -1002219005418,  
-    -1002447971680, 
+    -1002447971680,
     -1003079673697,
 ]
 
-@Client.on_message(filters.group)
+@app.on_message(filters.group)
 async def auto_leave_unauthorized(client, message):
     if message.chat.id not in ALLOWED_CHAT_IDS:
         await client.leave_chat(message.chat.id)
