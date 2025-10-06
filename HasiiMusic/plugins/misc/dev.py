78e39e0bd3ea6ef8d7084b9b9a3b1eef6e5a4135
Hasindu Nagolla <116471812+hasindu-nagolla@users.noreply.github.com>
2025-10-06 20:10:30 +0530

    configured auto leave funciton

--- HasiiMusic/plugins/misc/dev.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import filters
-from HasiiMusic import app
-
-@app.on_message(filters.command("sayyara") & filters.user(8172401675))  # Replace with your user ID
-async def leave_group(client, message):
-    await message.reply("Leaving group now.")
-    await client.leave_chat(-1002178211234)  # Replace with your group ID
