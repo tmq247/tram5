96d32b666e8776a236f0d789d1c90ad89e114d82
Hasindu Nagolla <116471812+hasindu-nagolla@users.noreply.github.com>
2025-10-14 08:20:35 +0530

    add initials

--- HasiiMusic/platforms/Apple.py
+++ HasiiMusic/platforms/Apple.py
@@ -3,9 +3,7 @@ from typing import List, Union, Optional
 
 import aiohttp
 from bs4 import BeautifulSoup
-#from youtubesearchpython.__future__ import VideosSearch
-from youtube_search import YoutubeSearch
-
+from youtubesearchpython.__future__ import VideosSearch
 
 
 class AppleAPI:
