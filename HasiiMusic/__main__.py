b165536a5596d48f6746871040169b2f35924364
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-10-21 05:38:36 +0700

    Fix logger namespace and update stream call URL

--- HasiiMusic/__main__.py
+++ HasiiMusic/__main__.py
@@ -48,12 +48,12 @@ async def init():
     for all_module in ALL_MODULES:
         importlib.import_module("HasiiMusic.plugins" + all_module)
 
-    LOGGER("Tune.plugins").info("ᴛᴜɴᴇ's ᴍᴏᴅᴜʟᴇs ʟᴏᴀᴅᴇᴅ...")
+    LOGGER("HasiiMusic.plugins").info("ᴛᴜɴᴇ's ᴍᴏᴅᴜʟᴇs ʟᴏᴀᴅᴇᴅ...")
     await userbot.start()
     await JARVIS.start()
 
     try:
-        await JARVIS.stream_call("https://files.catbox.moe/c3nt3q.mp4")
+        await JARVIS.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("Tune").error(
             "ᴘʟᴇᴀsᴇ ᴛᴜʀɴ ᴏɴ ᴛʜᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴏғ ʏᴏᴜʀ ʟᴏɢ ɢʀᴏᴜᴘ/ᴄʜᴀɴɴᴇʟ.\n\nᴀɴɴɪᴇ ʙᴏᴛ sᴛᴏᴘᴘᴇᴅ..."
