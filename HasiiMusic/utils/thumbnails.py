2133ba14a2151d7545c8b50713f19970a7ace8e6
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-11-13 23:04:50 +0700

    Update thumbnails.py

--- HasiiMusic/utils/thumbnails.py
+++ HasiiMusic/utils/thumbnails.py
@@ -3,7 +3,8 @@ import re
 import aiofiles
 import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
-from py_yt  import VideosSearch
+#from py_yt  import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch
 from config import YOUTUBE_IMG_URL
 from HasiiMusic.core.dir import CACHE_DIR 
 
