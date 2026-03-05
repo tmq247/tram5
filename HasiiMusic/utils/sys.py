e9f155ddc8a44187a12a8a2d63ad17abcffc66be
tmq247 <60788746+tmq247@users.noreply.github.com>
2026-03-05 19:25:40 +0700

    Update sys.py

--- HasiiMusic/utils/sys.py
+++ HasiiMusic/utils/sys.py
@@ -8,8 +8,20 @@ from HasiiMusic.utils.formatters import get_readable_time
 
 async def bot_sys_stats():
     bot_uptime = int(time.time() - _boot_)
-    UP = f"{get_readable_time(bot_uptime)}"
-    CPU = f"{psutil.cpu_percent(interval=0.5)}%"
-    RAM = f"{psutil.virtual_memory().percent}%"
-    DISK = f"{psutil.disk_usage('/').percent}%"
+    try:
+        UP = f"{get_readable_time(bot_uptime)}"
+    except:
+        UP = "N/A"
+    try:
+        CPU = f"{psutil.cpu_percent(interval=0.5)}%"
+    except:
+        CPU = "N/A"
+    try:
+        RAM = f"{psutil.virtual_memory().percent}%"
+    except:
+        RAM = "N/A"
+    try:
+        DISK = f"{psutil.disk_usage('/').percent}%"
+    except:
+        DISK = "N/A"
     return UP, CPU, RAM, DISK
