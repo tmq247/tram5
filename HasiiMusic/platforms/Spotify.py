db075b6b387ac8418e9d966a5592ddec330ed58a
tmq247 <60788746+tmq247@users.noreply.github.com>
2026-04-03 19:21:42 +0700

    Update Spotify.py

--- HasiiMusic/platforms/Spotify.py
+++ HasiiMusic/platforms/Spotify.py
@@ -3,7 +3,7 @@ import re
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 #from py_yt  import VideosSearch
-from youtubesearchpython.aio import VideosSearch
+#from youtubesearchpython.aio import VideosSearch
 import config
 
 
